93662e267c53a746d99618c007bd8e423ae8e3fa
faithwest <faithchepkoech425@gmail.com>
2023-09-19 12:38:14 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 *num2;
 
+const random = Math.floor(Math.random() * 100) + 1;
+let num3 = 20;
+let num4 = 8;
+let mod = (num3% num4);
+mod %= num4;
+
+let max = Math.floor(Math.max(20));
 // Write your code here
